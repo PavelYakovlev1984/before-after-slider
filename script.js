8c8dd90d49c8a2044493ca68810b9ec555a8296c
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-02-24 19:07:00 +0300

    js +

---
+++ script.js
@@ -0,0 +1,51 @@
+const slider = document.querySelector('.slider');
+const before = slider.querySelector('.before');
+const beforeImage = before.querySelector('.img');
+const change = slider.querySelector('.change');
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  let width = slider.offsetWidth;
+  beforeImage.style = `${width}px`;
+});
+
+const beforeAfterSlider = (x) => {
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = `${shift}px`;
+  change.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
+};  
+
+body.addEventListener('mousedown', () => {
+  isActive = true;
+  console.log('down');
+});
+
+body.addEventListener('mousemove', () => {
+  isActive = false;
+  console.log('up');
+});
+
+body.addEventListener('mouseleave', () => {
+  isActive = false;
+});
+
+body.addEventListener('mousemove', (e) => {
+  if (!isActive) {
+    return;
+  } 
+
+  let x = e.pageX;
+
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
+
